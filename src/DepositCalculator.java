04ca14029db397f4eafad9c9f7a83d78d073e161
Olga Obraztsova <ovobraztsova@yandex.ru>
2023-01-20 14:08:16 +0530

    Update DepositCalculator.java

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
+    // привет. В сигнатуре метода лучше будет указывать названия переменных более описательно. 
+    // Так код будет более понятным, в плане того, какие данных необходимо передавать.
     double calculateComplexPercent(double a, double y, int d ) {
         double pay = a * Math.pow((1 + y / 12), 12 * d);
         return getNumber (pay, 2);
     }
+    // между методами лучше добавлять одну строку, чтобы было удобнее читать.
     double calculateSimplePercent(double amount, double yearRate, int depositPeriod) {
         return getNumber(amount + amount * yearRate * depositPeriod, 2);
     }
+    // тут тоже :)
     double getNumber(double value, int places) {
         double ScaLe = Math.pow(10, places);
         return Math.round(value * ScaLe) / ScaLe;
+        // название переменной scale лучше сделать с маленькой буквы, тогда оно будет соответствовать правилу lowerCamelCase 
     }
 
     void makeDeposit( ) {
@@ -32,9 +37,13 @@ public class DepositCalculator {
         } else if (action == 2) {
             out = calculateComplexPercent(amount, 0.06, period);
         }
+        // и тут лучше сделать пустую строку
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + out);
     }
+    // пустая строка :)
     public static void main(String[] args) {
         new DepositCalculator().makeDeposit();
     }
+    
+    // а в остальном все отлично! :)
 }
